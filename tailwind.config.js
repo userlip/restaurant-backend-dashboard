bd424a050abd124bf2d9935ce1871ad182f648af
MadJack1906 <cho.suhoond.74@gmail.com>
2024-06-03 20:56:41 +0800

    Update tailwindcss package

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,25 @@ module.exports = {
     './config/**/*.php',
     './resources/**/*.{php,js}',
     './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
   ],
   theme: {
     extend: {
       colors: {
         primary: colors.blue,
       },
+      fontFamily: {
+        'alt': ['"Involve"', 'sans-serif'],
+      },
+      textColor: {
+        'body': '#5E5D5A'
+      },
+      screens: {
+        'tablet': '744px',
+        'big-tablet': '1000px',
+        'laptop': '1366px',
+        'desktop': '1920px',
+      }
     },
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
