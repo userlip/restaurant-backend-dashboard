16e10ba4564ba0099ad31b429a4d656851c5d5c1
MadJack1906 <54240101+MadJack1906@users.noreply.github.com>
2024-06-13 23:39:53 +0800

    Add preview to template 1

--- app/Filament/Resources/TemplateOneResource/Pages/EditTemplateOne.php
+++ app/Filament/Resources/TemplateOneResource/Pages/EditTemplateOne.php
@@ -3,17 +3,13 @@
 namespace App\Filament\Resources\TemplateOneResource\Pages;
 
 use App\Filament\Resources\TemplateOneResource;
+use App\Trait\TemplatePreviewActionTrait;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
 class EditTemplateOne extends EditRecord
 {
+    use TemplatePreviewActionTrait;
+
     protected static string $resource = TemplateOneResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-
-        ];
-    }
 }
