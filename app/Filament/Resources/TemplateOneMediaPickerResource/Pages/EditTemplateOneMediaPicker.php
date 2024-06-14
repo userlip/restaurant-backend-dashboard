9b822e67dd096fceb1ad58c5f7394704603370e6
MadJack1906 <54240101+MadJack1906@users.noreply.github.com>
2024-06-15 04:19:50 +0800

    Add use file upload redirect action for template one media picker

--- app/Filament/Resources/TemplateOneMediaPickerResource/Pages/EditTemplateOneMediaPicker.php
+++ app/Filament/Resources/TemplateOneMediaPickerResource/Pages/EditTemplateOneMediaPicker.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\TemplateOneMediaPickerResource\Pages;
 
 use App\Filament\Resources\TemplateOneMediaPickerResource;
+use App\Filament\Resources\TemplateOneResource;
+use App\Models\Website;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Colors\Color;
 
 class EditTemplateOneMediaPicker extends EditRecord
 {
@@ -13,7 +17,14 @@ class EditTemplateOneMediaPicker extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+             Action::make('use_file_upload')
+                ->color(Color::Emerald)
+                ->disabled(function () {
+                    return \Route::getCurrentRoute()->getName() === 'filament.admin.resources.template-ones.edit';
+                })
+                ->url(function (Website $record) {
+                    return TemplateOneResource::getUrl('edit', ['record' => $record->id]);
+                })
         ];
     }
 }
