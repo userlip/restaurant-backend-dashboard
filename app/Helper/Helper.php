f7d740dbd14942849452bc9cf8d2eee8485ef7a3
MadJack1906 <54240101+MadJack1906@users.noreply.github.com>
2024-06-18 02:33:03 +0800

    Add asset checker for Media and FileUpload assets

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -2,6 +2,8 @@
 
 namespace App\Helper;
 
+use Awcodes\Curator\Models\Media;
+
 class Helper
 {
     public static function getLanguageThreeLetterCode(string $lang)
@@ -12,5 +14,30 @@ class Helper
             "tr" => "TÜR",
             "ar" => "عربي",
         };
+    }
+
+    /**
+     * A function that will return the path of the asset from the storage
+     * In addition, to determine if an asset is a FileUpload or a Media asset
+     * You need to check if the value is string or an int.
+     *
+     * FileUpload : string = 'public/path/path.jpg'
+     * Media : int = 1 (Media::find(1)->url)
+     *
+     * @param string|int $asset
+     * @return string|null
+     */
+    public static function getAssetPath(string | int $asset) : string | null
+    {
+        // Checks if the asset has '/storage/' in its path as all assets
+        // including uploaded ones must be in the storage folder
+        if (! preg_match('/\\/storage\\//', $asset)) {
+            $asset = "/storage/" . $asset;
+        }
+
+        return is_string($asset)
+            ? asset($asset)
+            : Media::find($asset)
+                ?->url;
     }
 }
